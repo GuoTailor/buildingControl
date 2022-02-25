873aa83fdeb9c5a3e8a464d350455d373318fe6f
GYH <2350871838@qq.com>
2022-02-25 14:28:27 +0800

    first commit

---
+++ src/main/kotlin/com/gyh/buildingcontrol/BuildingControlApplication.kt
@@ -0,0 +1,11 @@
+package com.gyh.buildingcontrol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuildingControlApplication
+
+fun main(args: Array<String>) {
+    runApplication<BuildingControlApplication>(*args)
+}
---
+++ src/test/kotlin/com/gyh/buildingcontrol/BuildingControlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyh.buildingcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuildingControlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
